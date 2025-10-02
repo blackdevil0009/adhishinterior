09c0dadc37ef58e34f6b73a85a4becafbf7fae3b
blackdevil0009 <black0009devil@gmail.com>
2025-10-02 06:25:18 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,12 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "2007",
-  database: "contactdb"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
